39c52269ff5754b9c0907633d5e25d47be316180
Lievi77 <lguzm038@uottawa.ca>
2020-07-14 15:46:56 -0400

    Updates

---
+++ precache-manifest.1174939fb96e42f5c33f4b436a50853d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f6cd12b5bbaa62fccac687e12492d24",
+    "url": "/csl-LATAM-graph/index.html"
+  },
+  {
+    "revision": "717637a8d2e68514379f",
+    "url": "/csl-LATAM-graph/static/css/main.59b91bf8.chunk.css"
+  },
+  {
+    "revision": "6e789b2e1e79f900b62a",
+    "url": "/csl-LATAM-graph/static/js/2.6f9d6f58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/csl-LATAM-graph/static/js/2.6f9d6f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "717637a8d2e68514379f",
+    "url": "/csl-LATAM-graph/static/js/main.b7f546ec.chunk.js"
+  },
+  {
+    "revision": "8d378fd2d414824cccdd",
+    "url": "/csl-LATAM-graph/static/js/runtime-main.50c56c03.js"
+  }
+]);
